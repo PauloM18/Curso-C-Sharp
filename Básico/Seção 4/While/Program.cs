d764388752b96f014aa00e88272fa44122c60354
Paulo Henrique <111320952+PauloM18@users.noreply.github.com>
2023-06-17 21:18:17 -0300

    Atualizar

---
+++ Básico/Seção 4/While/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace While
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 10) 
+            {
+                Console.WriteLine("O valor da variavel: " + i);
+                i ++;
+
+            }
+            int x = 0;
+            bool repetir = true;
+            while (repetir)
+            {
+                Console.WriteLine("Valor da variavel X: " + x);
+                if(x > 20)
+                {
+                    Console.WriteLine("Saindo do programa!");
+                    repetir = false;
+                }
+                x++;   
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
