53886c6d2ac0378590d9822133f1f015e6a881f5
Paulo Henrique <111320952+PauloM18@users.noreply.github.com>
2023-06-19 12:36:55 -0300

    Atualizar

---
+++ Básico/Seção 5/Lista/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> marcas = new List<string> ();
+
+            marcas.Add("FIAT");
+            marcas.Add("Chevrolet");
+            marcas.Add("Renault");
+            marcas.Add("Pegeout");
+            marcas.Add("Volkswagen");
+
+            foreach (string marca in marcas) 
+            {
+                Console.WriteLine ("Marca: " + marca);
+            }
+            Console.WriteLine("Primeira Marca: " + marcas[0]);
+            Console.ReadKey();
+        }
+    }
+}
