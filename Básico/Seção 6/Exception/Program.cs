290eceaaf50e9a87de8ff581fcb6cebc1fd53502
Paulo Henrique <111320952+PauloM18@users.noreply.github.com>
2023-06-19 14:51:57 -0300

    Atualizar

---
+++ Básico/Seção 6/Exception/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número: ");
+            try
+            {
+                int numero = int.Parse(Console.ReadLine());
+                int num = int.Parse(null);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("Não foi digitado número!");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Tratado pela class Pai - Exeption");
+            }
+            finally
+            {
+                Console.WriteLine("Finally");
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
