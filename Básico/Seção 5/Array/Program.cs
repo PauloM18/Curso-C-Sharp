2dd37ffaafd266a6d772b1872a2b8a9b7ce7cabe
Paulo Henrique <111320952+PauloM18@users.noreply.github.com>
2023-06-19 11:25:36 -0300

    Atualizar

--- Básico/Seção 5/Array/Program.cs
+++ Básico/Seção 5/Array/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] marcas = { "Ferrari", "Google", "Coca-Cola" };
+            string[] nomes = new string[6];
+            nomes[0] = "jose";
+            nomes[1] = "Maria";
+        }
+    }
+}
