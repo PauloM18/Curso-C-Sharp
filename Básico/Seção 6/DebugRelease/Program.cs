993aa6e049d981bc165e5090563d48d48fc4d98e
Paulo Henrique <111320952+PauloM18@users.noreply.github.com>
2023-06-19 13:57:29 -0300

    Atualizar

---
+++ Básico/Seção 6/DebugRelease/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DebugRelease
+{
+    class Program
+    {
+        static void Main()
+        {
+            string nome = "jose";
+            Console.WriteLine("Hello, World!");
+
+            nome = "Elias";
+
+            Console.ReadKey();
+        }
+    }
+}
+
