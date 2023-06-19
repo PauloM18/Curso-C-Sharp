b7f52213c8cc87b71c67bdf39e168729324772d0
Paulo Henrique <111320952+PauloM18@users.noreply.github.com>
2023-06-19 11:03:26 -0300

    Atualizar

---
+++ Básico/Seção 4/Exercicio/Matematica.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio
+{
+    class Matematica
+    {
+       public static int Exponenciacao(int Base, int Exp)
+        {
+            int resultado = Base;
+            for (int i = 1; i < Exp; i++)
+            {
+                resultado = resultado * Base;
+            }
+            return resultado;
+        }
+        
+    }
+}
---
+++ Básico/Seção 4/Exercicio/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = Matematica.Exponenciacao(2, 4);
+            Console.WriteLine("2 elevado a 4 é " + valor);
+            Console.ReadKey();
+        }
+    }
+}
