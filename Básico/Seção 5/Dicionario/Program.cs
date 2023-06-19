bc33be857b7b3162a9af28af471224bfdd7b2810
Paulo Henrique <111320952+PauloM18@users.noreply.github.com>
2023-06-19 13:39:49 -0300

    Atualizar

---
+++ Básico/Seção 5/Dicionario/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Dicionario
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Dictionary<string, int> dicionario = new Dictionary<string, int>();
+            dicionario.Add("T1", 128);
+            dicionario.Add("t2", 121);
+            Console.WriteLine(dicionario["t2"]);
+        }
+    }
+}
