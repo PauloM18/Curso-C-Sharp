2def7cc1ad97735ecb222824d6e4f7a2c2bce094
Paulo Henrique <111320952+PauloM18@users.noreply.github.com>
2023-06-20 12:36:11 -0300

    Atualizar

--- Básico/Seção 7/Sobrecarga/Pessoa.cs
+++ Básico/Seção 7/Sobrecarga/Pessoa.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Sobrecarga
 {
-    internal class Pessoa
+    class Pessoa
     {
+        public int Andar()
+        {
+            return 1;
+        }
+        public int Andar(int lvl)
+        {
+            return Andar(1) * lvl;
+        }
+        public int Correr() 
+        {
+            return Andar() * 2;
+        }
+        public int Pedalar()
+        {
+            return Andar() * 4;
+        }
     }
 }
