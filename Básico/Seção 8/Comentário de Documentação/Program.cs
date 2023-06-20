805b7dc9e45e14b4e04eef77fa14f84b0d793166
Paulo Henrique <111320952+PauloM18@users.noreply.github.com>
2023-06-20 19:29:26 -0300

    Atualizar

---
+++ Básico/Seção 8/Comentário de Documentação/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Comentário_de_Documentação
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Veiculo veic = new Veiculo();
+            veic.Mover(1);
+        }
+    }
+}
---
+++ Básico/Seção 8/Comentário de Documentação/Veiculo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comentário_de_Documentação
+{
+    /// <summary>
+    /// Está classe é responsável por gerenciar todos os comportamento genéricos de um veiculo
+    /// </summary>
+    class Veiculo
+    {
+        /// <summary>
+        /// Marca do veiculo
+        /// </summary>
+        public string Marca;
+        /// <summary>
+        /// Método responsável por mover o veiculo
+        /// </summary>
+        /// <param name="medida"> 0 - para metros e 1- para km</param>
+        public void Mover(int medida)
+        {
+
+        }
+    }
+}
