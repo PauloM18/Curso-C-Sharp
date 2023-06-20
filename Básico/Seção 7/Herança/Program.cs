950d66e76f5d2e5e438a15d8becd08c1fd713a97
Paulo Henrique <111320952+PauloM18@users.noreply.github.com>
2023-06-20 11:21:40 -0300

    Atualizar

---
+++ Básico/Seção 7/Herança/Program.cs
@@ -0,0 +1,20 @@
+﻿using Heranca.Biblioteca;
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro();
+
+            Moto moto = new Moto();
+
+            Veiculo vei = new Veiculo();
+
+            carro.Marca = "FIAT";
+            moto.Marca = "Triumph";
+            vei.Marca = "Airbus";
+        }
+    }
+}
