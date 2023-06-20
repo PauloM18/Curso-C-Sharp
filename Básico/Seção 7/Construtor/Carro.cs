5e924f111a92f98dd82b313aad3a4640c4e57b9b
Paulo Henrique <111320952+PauloM18@users.noreply.github.com>
2023-06-20 14:49:48 -0300

    Atualizar

---
+++ Básico/Seção 7/Construtor/Carro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construtor
+{
+    internal class Carro
+    {
+        public string Marca;
+        public string Modelo;
+        public int Ano;
+        public Carro() 
+        {
+            Marca = "Fiat";
+            Modelo = "Palio";
+        }
+        public Carro(string Marca, string Modelo, int ano)
+        {
+            this.Marca = Marca;
+            this.Modelo = Modelo;
+            Ano = ano;
+        }
+    }
+}
---
+++ Básico/Seção 7/Construtor/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Construtor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro();
+            Carro carro2 = new Carro("Fiat", "Uno", 2010);
+        }
+    }
+}
